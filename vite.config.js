4afa5cc1638a1f22a80827b5ba12ac09f8fbd22c
tjdlwns33 <tjdlwns33@gmail.com>
2025-03-27 23:49:02 +0900

    chore: 404 이슈 해결

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,8 @@ import { defineConfig } from "vitest/config";
 
 export default defineConfig({
   test: {
+    base:
+      process.env.NODE_ENV === "production" ? "/front-5th-chapter1-1/" : "/",
     globals: true,
     environment: "jsdom",
     setupFiles: "./src/setupTests.js",
